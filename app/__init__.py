31415cbfd49cf6a90ade73a3467c14642f42943d
Cloud User <centos@ip-172-31-27-180.us-east-2.compute.internal>
2021-06-28 06:28:10 +0000

    adding login and register routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 import os
+from . import db
 from flask import Flask, render_template, request, send_from_directory, jsonify
 from dotenv import load_dotenv
-from . import db
+from werkzeug.security import generate_password_hash
+
 load_dotenv()
 
 app = Flask(__name__)
 app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
 db.init_app(app)
+from app.db import get_db
 
 @app.route('/')
 def index():
@@ -33,3 +36,58 @@ def resp():
     resp = jsonify(success=True)
     resp.status_code = 200
     return resp
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a restister page
+    return "Register Page not yet implemented", 501
+    
+    @app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501
